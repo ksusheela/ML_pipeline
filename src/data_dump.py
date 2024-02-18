38b4113b672f959780b5c8d4393a593314ad5847
Susheela kothakonda <sushe9sushe@gmail.com>
2024-02-18 23:52:51 +0100

    data dimp file complted

--- src/data_dump.py
+++ src/data_dump.py
@@ -7,16 +7,18 @@ client = "mongodb+srv://purnima11:Pavanpabba629@cluster0.twh81ir.mongodb.net/?re
 
 
 DATA_FILE_PATH = (r"")
-DATABASE = "Machine_Learning"
+DATABASE = "Machine_learning"
 COLLECTION_NAME = "DATASET"
 
 if __name__ == "__main__":
     df = pd.read_csv(DATA_FILE_PATH)
 
-    print("rows and colmns of our Data: {df.shape}")
-    df.reset_index(drop = True, inplace=True)
- 
-    json_record = list(json.loads(df.T.to_json()).values())
+    print(f"Rows and Columns of our Data: {df.shape}")
 
-    print(json_record[0])
-    client[DATABASE][COLLECTION_NAME].insert_many(json_record)
+    df.reset_index(drop = True, inplace = True)
+
+    joson_record = list(json.loads(df.T.to_json()).values())
+
+    print(joson_record[0])
+
+    client[DATABASE][COLLECTION_NAME].insert_many(joson_record)
